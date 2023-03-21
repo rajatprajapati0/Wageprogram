16a19dff4b187032405195892aa7490cd3cbbd0a
Rajat <rajatprajapati09@gmail.com>
2023-03-21 06:23:45 +0530

    calculating daily wage

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -7,14 +7,18 @@ namespace EmployeeWage
     public class EmployeeWage
     {
         const int isPresent = 1;
+        const int fullDayHour = 8;
+
         public void attendence() 
         {
+            int wagePerHour = 20;
             Random rnd = new Random();
             int empCheck=rnd.Next(0,2);
 
           if(empCheck == isPresent )
           {
-                Console.WriteLine("Employee is present");
+              int dailyWage= wagePerHour* fullDayHour;
+                Console.WriteLine($"Employee is present\nDaily Wage is:{dailyWage}");
           }
           else 
           {
