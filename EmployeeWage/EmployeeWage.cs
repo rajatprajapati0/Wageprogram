ef426d24d5ac1d487b76e071b1cebc2d41e0ba03
Rajat <rajatprajapati09@gmail.com>
2023-03-23 02:09:46 +0530

    refactored code switch case

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System;
+﻿using System;
 
 namespace EmployeeWage
 {
@@ -9,29 +7,32 @@ namespace EmployeeWage
         const int fullDayPresent = 1;
         const int halfDayPresent = 2;
 
-        const int fullDayHour = 8;
-        const int halfDayHour = 4;
-
         public void attendence() 
         {
+            int dailyWage = 0;
+            int empHour = 0;
             int wagePerHour = 20;
             Random rnd = new Random();
             int empCheck=rnd.Next(0,3);
 
-          if(empCheck == fullDayPresent)
-          {
-              int dailyWage= wagePerHour* fullDayHour;
-                Console.WriteLine($"Employee is present for full day\nDaily Wage is:{dailyWage}");
-          }
-          else if(empCheck== halfDayPresent)
-          {
-                int dailyWage = wagePerHour * halfDayHour;
-                Console.WriteLine($"Employee is present for half day\nDaily Wage is:{dailyWage}");
-          }
-          else 
-          {
-                Console.WriteLine("Employee is not present");
-          }
+            switch (empCheck)
+            {
+                case fullDayPresent: empHour = 8; 
+                    
+                    break;
+
+                case halfDayPresent: empHour = 4;
+                    
+                    break;
+
+                default            : empHour=0;
+                    
+                    break;
+            }
+
+            dailyWage = empHour * wagePerHour;
+            Console.WriteLine($"Daily Wage is:{dailyWage}");
+                  
         }
     }
 }
