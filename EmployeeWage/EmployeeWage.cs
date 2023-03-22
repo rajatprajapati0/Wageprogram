9c1d396b95e358db5e6042840a2f534ed5701db0
Rajat <rajatprajapati09@gmail.com>
2023-03-23 02:27:37 +0530

    calculating emp wage for 20 days

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -6,33 +6,34 @@ namespace EmployeeWage
     {
         const int fullDayPresent = 1;
         const int halfDayPresent = 2;
-
+        const int maxDay = 20;
         public void attendence() 
         {
-            int dailyWage = 0;
+            int dailyWage;
             int empHour = 0;
             int wagePerHour = 20;
             Random rnd = new Random();
-            int empCheck=rnd.Next(0,3);
-
-            switch (empCheck)
+          
+            for(int i=0;i<maxDay;i++)
             {
-                case fullDayPresent: empHour = 8; 
-                    
-                    break;
+                int empCheck = rnd.Next(0, 3);
+                switch (empCheck)
+                {
+                    case fullDayPresent:
+                        empHour += 8;
+                        break;
 
-                case halfDayPresent: empHour = 4;
-                    
-                    break;
+                    case halfDayPresent:
+                        empHour += 4;
+                        break;
 
-                default            : empHour=0;
-                    
-                    break;
+                    default:
+                        empHour += 0;
+                        break;
+                }
             }
-
             dailyWage = empHour * wagePerHour;
             Console.WriteLine($"Daily Wage is:{dailyWage}");
-                  
         }
     }
 }
