f26b24d966a60bcd62f4fb91a7ebd1ef2f42322c
Rajat <rajatprajapati09@gmail.com>
2023-03-23 01:49:51 +0530

    calculating Emp wage for half day

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -6,19 +6,27 @@ namespace EmployeeWage
 {
     public class EmployeeWage
     {
-        const int isPresent = 1;
+        const int fullDayPresent = 1;
+        const int halfDayPresent = 2;
+
         const int fullDayHour = 8;
+        const int halfDayHour = 4;
 
         public void attendence() 
         {
             int wagePerHour = 20;
             Random rnd = new Random();
-            int empCheck=rnd.Next(0,2);
+            int empCheck=rnd.Next(0,3);
 
-          if(empCheck == isPresent )
+          if(empCheck == fullDayPresent)
           {
               int dailyWage= wagePerHour* fullDayHour;
-                Console.WriteLine($"Employee is present\nDaily Wage is:{dailyWage}");
+                Console.WriteLine($"Employee is present for full day\nDaily Wage is:{dailyWage}");
+          }
+          else if(empCheck== halfDayPresent)
+          {
+                int dailyWage = wagePerHour * halfDayHour;
+                Console.WriteLine($"Employee is present for half day\nDaily Wage is:{dailyWage}");
           }
           else 
           {
