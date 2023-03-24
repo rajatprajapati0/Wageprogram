dd8485026ab2ce2146edfbc0f6537fad69c01fd2
Rajat <rajatprajapati09@gmail.com>
2023-03-25 02:27:35 +0530

    counting total hours till maxHour reched at 100

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -7,33 +7,41 @@ namespace EmployeeWage
         const int fullDayPresent = 1;
         const int halfDayPresent = 2;
         const int maxDay = 20;
+        const int maxHour = 100;
         public void attendence() 
         {
-            int dailyWage;
-            int empHour = 0;
+
+            int dailyWage, empHour = 0, totalday = 0,totalEmpHour=0;
+           
             int wagePerHour = 20;
             Random rnd = new Random();
-          
-            for(int i=0;i<maxDay;i++)
+
+            while (empHour <= maxHour && totalday <maxDay) 
             {
+               
                 int empCheck = rnd.Next(0, 3);
                 switch (empCheck)
                 {
                     case fullDayPresent:
-                        empHour += 8;
+                        empHour = 8;
                         break;
 
                     case halfDayPresent:
-                        empHour += 4;
+                        empHour = 4;
+                        
                         break;
 
                     default:
-                        empHour += 0;
+                        empHour = 0;
                         break;
                 }
+                totalEmpHour += empHour;
+                totalday++;
+                Console.WriteLine($"Emp Hour:{empHour}\nTotal Day{totalday}");
+
             }
-            dailyWage = empHour * wagePerHour;
-            Console.WriteLine($"Daily Wage is:{dailyWage}");
+            dailyWage = totalEmpHour * wagePerHour;
+            Console.WriteLine($"Daily Wage is:{dailyWage}\nTotal Hour:{totalEmpHour}\nTotal Day{totalday}");
         }
     }
 }
